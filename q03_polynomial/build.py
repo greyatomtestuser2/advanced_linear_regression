35f06eca49831cfdf7ced5cb0534069b2a90454c
greyatomtestuser2 <greyatomtestuser2@gmail.com>
2018-04-30 12:10:06 +0000

    update

--- q03_polynomial/build.py
+++ q03_polynomial/build.py
@@ -3,9 +3,48 @@ from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LinearRegression
-
-# We have already loaded the data for you
+import numpy as np
+np.random.seed(9)
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
-
+from greyatomlib.advanced_linear_regression.q02_Max_important_feature.build import Max_important_feature
 
 # Write your solution here
+# def polynomial(power=5,random_state=9):
+#     Poly_Xtrain = X_train[['OverallQual', 'GrLivArea', 'GarageCars', 'GarageArea']]
+#     Poly_ytrain = y_train
+#     poly_features = make_pipeline(PolynomialFeatures(power, include_bias=False), LinearRegression())
+#     model = poly_features.fit(Poly_Xtrain, Poly_ytrain)
+#     return model
+
+# model = polynomial()
+# prediction = model.predict(np.array([4, 5, 6, 7]).reshape(1, -1))
+#
+# print(prediction)
+
+
+
+# def polynomial (power= 5,random_state = 9):
+#     features = list(Max_important_feature(data_set))
+#     #return poly.fit_transform(data_set[features])
+#     poly_model = make_pipeline(PolynomialFeatures(power,include_bias=False),
+#                            LinearRegression())
+#     poly_model.fit( X_train[['OverallQual', 'GrLivArea', 'GarageCars', 'GarageArea']],y_train)
+#     #poly_model.fit(X_train[features],y_train)
+#     return poly_model
+#
+#
+# model = polynomial()
+# prediction = model.predict(np.array([4, 5, 6, 7]).reshape(1, -1))
+# print (np.round_(prediction,2), np.array([np.round_(32740.9,2)]))
+
+
+def polynomial(power=5,random_state=9):
+    Poly_Xtrain = X_train.iloc[:,[2,14,24,25]]
+    Poly_ytrain = y_train
+    poly_features = make_pipeline(PolynomialFeatures(power, include_bias=False), LinearRegression())
+    model = poly_features.fit(Poly_Xtrain, Poly_ytrain)
+    return model
+
+model = polynomial()
+prediction = model.predict(np.array([4, 5, 6, 7]).reshape(1, -1))
+print (np.round_(prediction,2), np.array([np.round_(32740.9,2)]))
